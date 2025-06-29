ebebd1856ca0691cf7cfcbaf3889f98a1dfcc092
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-06-29 23:56:17 +0545

    refactor: rename AuthRepository to IAuthRepository and update logout return type to void

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:ilike/core/error/failures.dart';
 import 'package:ilike/features/auth/domain/entities/user_entity.dart';
 
-abstract class AuthRepository {
+abstract interface class IAuthRepository {
   // Authenticate a user with email and password
   Future<Either<Failure, UserEntity>> login(String email, String password);
   
@@ -10,7 +10,7 @@ abstract class AuthRepository {
   Future<Either<Failure, UserEntity>> register(String name, String email, String password);
   
   // Logout the current user
-  Future<Either<Failure, Unit>> logout();
+  Future<Either<Failure, void>> logout();
   
   // Get the currently authenticated user
   Future<Either<Failure, UserEntity>> getCurrentUser();
