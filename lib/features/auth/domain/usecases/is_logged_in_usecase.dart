0297d94ceaf6d022de065e6a14b4b4a9d5780d98
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-07-01 23:32:19 +0545

    implement isloggedin usecase

--- lib/features/auth/domain/usecases/is_logged_in_usecase.dart
+++ lib/features/auth/domain/usecases/is_logged_in_usecase.dart
@@ -1,23 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:ilike/core/error/failures.dart';
+import 'package:ilike/core/usecase/usecase.dart';
 import 'package:ilike/features/auth/domain/repositories/auth_repository.dart';
 
 /// Use case for checking if a user is currently logged in
-class IsLoggedInUseCase {
-  final AuthRepository repository;
+class IsLoggedInUseCase implements UsecaseWithoutParams<bool> {
+  final IAuthRepository repository;
 
   IsLoggedInUseCase(this.repository);
 
-  /// Checks if a user is currently logged in
-  /// 
-  /// Returns [bool] indicating login status
-  /// Returns [Failure] if there's an error checking the status
+  @override
   Future<Either<Failure, bool>> call() async {
-    try {
-      final result = await repository.isLoggedIn();
-      return result;
-    } catch (e) {
-      return Left(ServerFailure('Failed to check login status'));
-    }
+    return await repository.isLoggedIn();
   }
 }
