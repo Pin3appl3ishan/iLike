c3773db74a83ad4678fddd7a9fcab1e596a285d2
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-07-03 21:32:41 +0545

    feat: create IProfileRepository interface for profile management operations

---
+++ lib/features/profile/domain/repositories/profile_repository.dart
@@ -0,0 +1,12 @@
+import 'package:ilike/core/error/failures.dart';
+import 'package:ilike/features/profile/domain/entities/profile_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract interface class IProfileRepository {
+  Future<Either<Failure, ProfileEntity?>> getProfile();
+  Future<Either<Failure, void>> saveProfile(ProfileEntity profile);
+  Future<Either<Failure, void>> updateProfile(ProfileEntity profile);
+  Future<Either<Failure, void>> deleteProfile();
+  Future<Either<Failure, String>> uploadPhoto(String imagePath);
+  Future<Either<Failure, void>> deletePhoto(String photoUrl);
+}
