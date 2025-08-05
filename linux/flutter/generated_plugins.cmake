41b0f3cc471ccf2cdc2462d3fa79bf5ec0e79f86
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-08-05 11:02:42 +0545

    chore: update Linux and macOS platform configurations

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,8 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  audioplayers_linux
   file_selector_linux
-  flutter_secure_storage_linux
+  native_splash_screen_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
